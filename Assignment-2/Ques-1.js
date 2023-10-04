aa773e425d048cc241854e583bfaefc2fe5e75a4
vinay1-successive <vinay.tomar@successive.tech>
2023-10-04 18:50:36 +0530

    updated code

--- Assignment-2/Ques-1.js
+++ Assignment-2/Ques-1.js
@@ -1,8 +1,17 @@
 // 1. Write a program to give a random output between 1 to 10 (AC: 2 simultaneous answers could not be same)
 
-function get_Random() {
-  return Math.round(Math.random() * 10 + 1, 2);
+
+var x=-1;
+const get_Random =()=> {
+  let number=Math.floor(Math.random()*10+1);
+  while(number===x)
+  {
+    number=Math.floor(Math.random()*10+1);
+  }
+  x=number;
+  return number;
 }
 
-const random_number = get_Random();
-console.log(random_number);
+const random_number1 = get_Random();
+const random_number2 = get_Random();
+console.log(random_number1,random_number2);
